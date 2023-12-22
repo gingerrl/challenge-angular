eb683c32bb8ceaf0a020cc357e51abef7bed0847
unknown <ginger1881@hotmail.com>
2023-12-21 22:20:53 -0500

    feat: add components modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { MainViewComponent } from './views/main-view/main-view.component';
+import { CardFormComponent } from './views/card-form/card-form.component';
+import { AppRoutingModule } from './routes/routing.module';
+import { TableListComponent } from './components/table-list/table-list.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainViewComponent,
+    CardFormComponent,
+    TableListComponent,
+    SearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/routes/routing.module.ts
@@ -0,0 +1,34 @@
+import { Routes, RouterModule } from "@angular/router";
+import { CardFormComponent } from "../views/card-form/card-form.component";
+import { MainViewComponent } from "../views/main-view/main-view.component";
+import { NgModule } from "@angular/core";
+
+
+const routes: Routes = [
+    {
+        path: 'home',
+        component: MainViewComponent,
+    },
+    {
+        path: 'form',
+        component: CardFormComponent,
+    },
+    {
+        path: 'form/:id',
+        component: CardFormComponent,
+    },
+    {
+        path: '**',
+        redirectTo: 'home'
+    }
+]
+@NgModule({
+    imports: [
+        RouterModule.forRoot(routes)
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+
+export class AppRoutingModule { }
