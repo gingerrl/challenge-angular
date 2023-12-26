35052a1d499fc1135a89dfc12ae0778cfb237a17
unknown <ginger1881@hotmail.com>
2023-12-26 14:16:48 -0500

    feat: add file endPoints

---
+++ src/app/config/endpoints.enum.ts
@@ -0,0 +1,5 @@
+export enum Endpoints {
+    URL = "https://tribu-ti-staffing-desarrollo-afangwbmcrhucqfh.z01.azurefd.net/ipf-msa-productosfinancieros/bp/products",
+    VERIFICATION = "https://tribu-ti-staffing-desarrollo-afangwbmcrhucqfh.z01.azurefd.net/ipf-msa-productosfinancieros/bp/products/verification/"
+    
+}
