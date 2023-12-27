782ada1d354d066ec6b593d3a39c4558a3573a6f
unknown <ginger1881@hotmail.com>
2023-12-26 22:05:18 -0500

    refactor: update component table-list

--- src/app/components/table-list/table-list.component.ts
+++ src/app/components/table-list/table-list.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
 import { ListCard } from 'src/app/models/data-list.models';
 
 @Component({
@@ -8,13 +8,21 @@ import { ListCard } from 'src/app/models/data-list.models';
   styleUrls: ['./table-list.component.css']
 })
 export class TableListComponent implements OnInit, OnChanges {
-  @Input() dataList: ListCard[] = []
+  @Input() dataList: ListCard[] = [];
+  @Output() listEvent = new EventEmitter()
 
   listFilter: ListCard[] = []
   showModalDelete = false
-  showModalEdit = false
+  selectList: ListCard = {
+    id: '',
+    name: '',
+    description: '',
+    logo: '',
+    date_release: '',
+    date_revision: '',
+  }
   constructor(
-    private router: Router
+    private router: Router,
 
   ) { }
   ngOnInit(): void {
@@ -22,17 +30,30 @@ export class TableListComponent implements OnInit, OnChanges {
 
   handleSelect(e: any) {
     this.listFilter = this.dataList.slice(0, e.target.value)
-    console.log(e.target.value)
   }
 
   ngOnChanges(changes: SimpleChanges): void {
     this.listFilter = changes?.['dataList'].currentValue.slice(0, 5)
   }
-  handleEdit() {
-    this.showModalEdit = true
-    this.router.navigate(['/form'])
+  handleEdit(item: ListCard) {
+    const navigationData: NavigationExtras = {
+      state: {
+        item
+      }
+    }
+    this.router.navigate(['/form'], navigationData)
   }
-  handleDelete() {
+  handleDelete(item: ListCard) {
+    this.selectList = item
     this.showModalDelete = true
+  }
+
+  handleCloseModal() {
+    this.showModalDelete = false
+
+  }
+  handleConfirm() {
+    this.showModalDelete = false;
+    this.listEvent.emit()
   }
 }
